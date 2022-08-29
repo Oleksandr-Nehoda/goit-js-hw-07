b00366eab0c9d8a108f9a5a3fe9510f6ff3e04ad
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-08-29 17:35:20 +0300

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryConteinerRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCards(galleryItems);
+
+
+galleryConteinerRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryConteinerRef.addEventListener('click', onOpenLightbox);
+
+function onOpenLightbox(event) {
+
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG'){
+        return
+    }
+    var lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionDelay: '250',
+ });
+}
+
+function createGalleryCards (galleryItems) {
+    return galleryItems.map( ({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+    }).join("");
+}
+
