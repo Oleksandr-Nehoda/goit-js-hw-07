edce32f5196d62d0d35bcefa297ff3632503a2ae
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-08-29 08:56:55 +0300

    markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryConteiner = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCards(galleryItems);
+
+galleryConteiner.insertAdjacentHTML('beforeend', galleryMarkup);
+
+console.log(galleryMarkup);
+
+
+function createGalleryCards (galleryItems) {
+    return galleryItems.map( ({preview, original, description}) => {
+    return `
+        <div class="gallery__item">
+           <a class="gallery__link" href="${original}">
+              <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+          />
+          </a>
+       </div>`
+    }).join("");
+
+}
